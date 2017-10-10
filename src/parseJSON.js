4e5a91ecc404510c93e151602360c36c699cac38
Brian Binder <bbinder912@gmail.com>
2017-10-09 21:27:48 -0700

    Build parseJSON - escape characters cause issues

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,6 +2,10 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
+
+// '{"foo": true, "bar": false, "baz": null}'
+
+
 var parseJSON = function(json) {
   var curChar = json[0];
 
@@ -9,12 +13,17 @@ var parseJSON = function(json) {
     nextCharacter();
     var obj = {};
     var parsePairs = function() {
+      if (curChar === ' ') {
+        nextCharacter();
+      }
       var colonPosition = json.indexOf(':');
-      var key = json.slice(0, colonPosition);
+      var key = json.slice(1, colonPosition - 1);
       json = json.slice(colonPosition + 1);
+      curChar = json[0];
       var value = parseValue();
       obj[key] = value;
       while (curChar === ',') {
+        nextCharacter();
         parsePairs();
       }
     };
@@ -48,6 +57,9 @@ var parseJSON = function(json) {
   };
 
   var parseValue = function() {
+    if (curChar === ' ') {
+      nextCharacter();
+    }
     //string
     if (curChar === '"') {
       nextCharacter();
@@ -121,6 +133,204 @@ var parseJSON = function(json) {
 
 };
 
+var testJSONParse = function(string) {
+  var expected = JSON.parse(string);
+  var result = parseJSON(string);
+  if (JSON.stringify(expected) === JSON.stringify(result)) {
+    console.log('Test passed');
+  } else {
+    console.log('Expected ' + JSON.stringify(result) + ' to equal '
+      + JSON.stringify(expected));
+  }
+};
+
+var testArray = function(fixStrings) {
+  fixStrings.forEach(function(string) {
+    testJSONParse(string);
+  });
+};
+
+var basicStrings = [
+  '[]',
+  '{"foo": ""}',
+  '{}',
+  '{"foo": "bar"}',
+  '["one", "two"]',
+  '{"a": "b", "c": "d"}',
+  '[null,false,true]',
+  '{"foo": true, "bar": false, "baz": null}',
+  '[1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999]',
+  '{"boolean, true": true, "boolean, false": false, "null": null }'
+];
+
+var nestedStrings = [
+  '{"a":{"b":"c"}}',
+  '{"a":["b", "c"]}',
+  '[{"a":"b"}, {"c":"d"}]',
+  '{"a":[],"c": {}, "b": true}',
+  '[[[["foo"]]]]'
+];
+
+var escapingStrings = [
+  '["\\\\\\"\\"a\\""]',
+  '["and you can\'t escape thi\s"]'
+];
+
+
+
+var everythingStrings = [
+  '{"CoreletAPIVersion":2,"CoreletType":"standalone",' +
+    '"documentation":"A corelet that provides the capability to upload' +
+    ' a folderâ€™s contents into a userâ€™s locker.","functions":[' +
+    '{"documentation":"Displays a dialog box that allows user to ' +
+    'select a folder on the local system.","name":' +
+    '"ShowBrowseDialog","parameters":[{"documentation":"The ' +
+    'callback function for results.","name":"callback","required":' +
+    'true,"type":"callback"}]},{"documentation":"Uploads all mp3 files' +
+    ' in the folder provided.","name":"UploadFolder","parameters":' +
+    '[{"documentation":"The path to upload mp3 files from."' +
+    ',"name":"path","required":true,"type":"string"},{"documentation":' +
+    ' "The callback function for progress.","name":"callback",' +
+    '"required":true,"type":"callback"}]},{"documentation":"Returns' +
+    ' the server name to the current locker service.",' +
+    '"name":"GetLockerService","parameters":[]},{"documentation":' +
+    '"Changes the name of the locker service.","name":"SetLockerSer' +
+    'vice","parameters":[{"documentation":"The value of the locker' +
+    ' service to set active.","name":"LockerService","required":true' +
+    ',"type":"string"}]},{"documentation":"Downloads locker files to' +
+    ' the suggested folder.","name":"DownloadFile","parameters":[{"' +
+    'documentation":"The origin path of the locker file.",' +
+    '"name":"path","required":true,"type":"string"},{"documentation"' +
+    ':"The Window destination path of the locker file.",' +
+    '"name":"destination","required":true,"type":"integer"},{"docum' +
+    'entation":"The callback function for progress.","name":' +
+    '"callback","required":true,"type":"callback"}]}],' +
+    '"name":"LockerUploader","version":{"major":0,' +
+    '"micro":1,"minor":0},"versionString":"0.0.1"}',
+  '{ "firstName": "John", "lastName" : "Smith", "age" : ' +
+    '25, "address" : { "streetAddress": "21 2nd Street", ' +
+    '"city" : "New York", "state" : "NY", "postalCode" : ' +
+    ' "10021" }, "phoneNumber": [ { "type" : "home", ' +
+    '"number": "212 555-1234" }, { "type" : "fax", ' +
+    '"number": "646 555-4567" } ] }',
+  '{\r\n' +
+    '          "glossary": {\n' +
+    '              "title": "example glossary",\n\r' +
+    '      \t\t"GlossDiv": {\r\n' +
+    '                  "title": "S",\r\n' +
+    '      \t\t\t"GlossList": {\r\n' +
+    '                      "GlossEntry": {\r\n' +
+    '                          "ID": "SGML",\r\n' +
+    '      \t\t\t\t\t"SortAs": "SGML",\r\n' +
+    '      \t\t\t\t\t"GlossTerm": "Standard Generalized ' +
+    'Markup Language",\r\n' +
+    '      \t\t\t\t\t"Acronym": "SGML",\r\n' +
+    '      \t\t\t\t\t"Abbrev": "ISO 8879:1986",\r\n' +
+    '      \t\t\t\t\t"GlossDef": {\r\n' +
+    '                              "para": "A meta-markup language,' +
+    ' used to create markup languages such as DocBook.",\r\n' +
+    '      \t\t\t\t\t\t"GlossSeeAlso": ["GML", "XML"]\r\n' +
+    '                          },\r\n' +
+    '      \t\t\t\t\t"GlossSee": "markup"\r\n' +
+    '                      }\r\n' +
+    '                  }\r\n' +
+    '              }\r\n' +
+    '          }\r\n' +
+    '      }\r\n'
+];
+
+parseableStrings = [
+  // basic stuff
+  '[]',
+  '{"foo": ""}',
+  '{}',
+  '{"foo": "bar"}',
+  '["one", "two"]',
+  '{"a": "b", "c": "d"}',
+  '[null,false,true]',
+  '{"foo": true, "bar": false, "baz": null}',
+  '[1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999]',
+  '{"boolean, true": true, "boolean, false": false, "null": null }',
+
+  // basic nesting
+  '{"a":{"b":"c"}}',
+  '{"a":["b", "c"]}',
+  '[{"a":"b"}, {"c":"d"}]',
+  '{"a":[],"c": {}, "b": true}',
+  '[[[["foo"]]]]',
+
+  // escaping
+  '["\\\\\\"\\"a\\""]',
+  '["and you can\'t escape thi\s"]',
+
+  // everything all at once
+  '{"CoreletAPIVersion":2,"CoreletType":"standalone",' +
+    '"documentation":"A corelet that provides the capability to upload' +
+    ' a folderâ€™s contents into a userâ€™s locker.","functions":[' +
+    '{"documentation":"Displays a dialog box that allows user to ' +
+    'select a folder on the local system.","name":' +
+    '"ShowBrowseDialog","parameters":[{"documentation":"The ' +
+    'callback function for results.","name":"callback","required":' +
+    'true,"type":"callback"}]},{"documentation":"Uploads all mp3 files' +
+    ' in the folder provided.","name":"UploadFolder","parameters":' +
+    '[{"documentation":"The path to upload mp3 files from."' +
+    ',"name":"path","required":true,"type":"string"},{"documentation":' +
+    ' "The callback function for progress.","name":"callback",' +
+    '"required":true,"type":"callback"}]},{"documentation":"Returns' +
+    ' the server name to the current locker service.",' +
+    '"name":"GetLockerService","parameters":[]},{"documentation":' +
+    '"Changes the name of the locker service.","name":"SetLockerSer' +
+    'vice","parameters":[{"documentation":"The value of the locker' +
+    ' service to set active.","name":"LockerService","required":true' +
+    ',"type":"string"}]},{"documentation":"Downloads locker files to' +
+    ' the suggested folder.","name":"DownloadFile","parameters":[{"' +
+    'documentation":"The origin path of the locker file.",' +
+    '"name":"path","required":true,"type":"string"},{"documentation"' +
+    ':"The Window destination path of the locker file.",' +
+    '"name":"destination","required":true,"type":"integer"},{"docum' +
+    'entation":"The callback function for progress.","name":' +
+    '"callback","required":true,"type":"callback"}]}],' +
+    '"name":"LockerUploader","version":{"major":0,' +
+    '"micro":1,"minor":0},"versionString":"0.0.1"}',
+  '{ "firstName": "John", "lastName" : "Smith", "age" : ' +
+    '25, "address" : { "streetAddress": "21 2nd Street", ' +
+    '"city" : "New York", "state" : "NY", "postalCode" : ' +
+    ' "10021" }, "phoneNumber": [ { "type" : "home", ' +
+    '"number": "212 555-1234" }, { "type" : "fax", ' +
+    '"number": "646 555-4567" } ] }',
+  '{\r\n' +
+    '          "glossary": {\n' +
+    '              "title": "example glossary",\n\r' +
+    '      \t\t"GlossDiv": {\r\n' +
+    '                  "title": "S",\r\n' +
+    '      \t\t\t"GlossList": {\r\n' +
+    '                      "GlossEntry": {\r\n' +
+    '                          "ID": "SGML",\r\n' +
+    '      \t\t\t\t\t"SortAs": "SGML",\r\n' +
+    '      \t\t\t\t\t"GlossTerm": "Standard Generalized ' +
+    'Markup Language",\r\n' +
+    '      \t\t\t\t\t"Acronym": "SGML",\r\n' +
+    '      \t\t\t\t\t"Abbrev": "ISO 8879:1986",\r\n' +
+    '      \t\t\t\t\t"GlossDef": {\r\n' +
+    '                              "para": "A meta-markup language,' +
+    ' used to create markup languages such as DocBook.",\r\n' +
+    '      \t\t\t\t\t\t"GlossSeeAlso": ["GML", "XML"]\r\n' +
+    '                          },\r\n' +
+    '      \t\t\t\t\t"GlossSee": "markup"\r\n' +
+    '                      }\r\n' +
+    '                  }\r\n' +
+    '              }\r\n' +
+    '          }\r\n' +
+    '      }\r\n'
+];
+
+// JSON does not allow you to parse these strings
+unparseableStrings = [
+  '["foo", "bar"',
+  '["foo", "bar\\"]'
+];
+
+testArray(escapingStrings);
 
 // object
 //     {}
